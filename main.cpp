1f5e780b906c9906a7a8add74ab2fe102e99be2f
Javier Saldana <javier@tractical.com>
2013-04-25 23:50:19 -0500

    refactor image class

--- main.cpp
+++ main.cpp
@@ -14,15 +14,15 @@
 #endif
 
 #include <iostream>
+#include <string>
+#include <sstream>
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
-#include <string>
-#include <sstream>
 #include <time.h>
-#include "imageloader.h"
 
-using namespace std;
+#include "image.h"
 
 /*
    Ventana
@@ -114,38 +114,6 @@ double snake[100+2][2] = {{0,0},{0,1}};
 /** -- Fin de las variables -- **/
 
 
-/*
- Librería para manejo de texturas
- ********************************/
-
-void loadTexture(Image* image,int k)
-{
-
-    glBindTexture(GL_TEXTURE_2D, texName[k]); //Tell OpenGL which texture to edit
-
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,GL_NEAREST);
-
-
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,GL_LINEAR);
-
-    //Map the image to the texture
-    glTexImage2D(GL_TEXTURE_2D,                //Always GL_TEXTURE_2D
-                 0,                            //0 for now
-                 GL_RGB,                       //Format OpenGL uses for image
-                 image->width, image->height,  //Width and height
-                 0,                            //The border of the image
-                 GL_RGB, //GL_RGB, because pixels are stored in RGB format
-                 GL_UNSIGNED_BYTE, //GL_UNSIGNED_BYTE, because pixels are stored
-                 //as unsigned numbers
-                 image->pixels);               //The actual pixel data
-
-}
-
-
 /********************************************************************************
  ********************************************************************************
  **                                                                            **
@@ -178,6 +146,32 @@ void loadTexture(Image* image,int k)
  ********************************************************************************
  ********************************************************************************/
 
+void loadTexture(Image* image,int k) {
+    glBindTexture(GL_TEXTURE_2D, texName[k]); // Tell OpenGL which texture to edit
+    
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,GL_NEAREST);
+    
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,GL_LINEAR);
+    
+    //Map the image to the texture
+    glTexImage2D(GL_TEXTURE_2D,               // Always GL_TEXTURE_2D
+                 0,                           // 0 for now
+                 GL_RGB,                      // Format OpenGL uses for image
+                 image->width, image->height, // Width and height
+                 0,                           // The border of the image
+                 GL_RGB,                      // GL_RGB, because pixels are stored in RGB format
+                 GL_UNSIGNED_BYTE,            // GL_UNSIGNED_BYTE, because pixels are stored
+                 //   as unsigned numbers
+                 image->pixels);              // The actual pixel data
+}
+
+// Reads a bitmap image from a file
+Image* loadBMP(const char* filename);
 
 /*
  *
@@ -335,7 +329,7 @@ static void drawMap(void)
     // Dibuja el Marcador
     glColor3f(1.0, 1.0, 1.0);
 
-    stringstream ss; // Helper para desplegar el marcador
+    std::stringstream ss; // Helper para desplegar el marcador
 
     ss << "Score: " << std::to_string(score);
     drawString(GLUT_BITMAP_9_BY_15, ss.str().c_str(), -0.85, -0.85);
@@ -418,7 +412,7 @@ static void drawPerspective(void)
     // Dibuja el Marcador
     glColor3f(1.0, 1.0, 1.0);
 
-    stringstream ss; // Helper para desplegar el marcador
+    std::stringstream ss; // Helper para desplegar el marcador
 
     ss << "Score: " << std::to_string(score);
     drawString(GLUT_BITMAP_9_BY_15, ss.str().c_str(), -0.85, -0.85);
@@ -598,210 +592,4 @@ int main(int argc, char *argv[])
 
     glutMainLoop();
     return EXIT_SUCCESS;
-}
-
-
-/*
- Librería para manejo de imágenes
- ********************************/
-
-#include <assert.h>
-#include <fstream>
-
-Image::Image(char* ps, int w, int h) : pixels(ps), width(w), height(h)
-{
-
-}
-
-Image::~Image()
-{
-    delete[] pixels;
-}
-
-namespace
-{
-    //Converts a four-character array to an integer, using little-endian form
-    int toInt(const char* bytes)
-    {
-        return (int)(((unsigned char)bytes[3] << 24) |
-                     ((unsigned char)bytes[2] << 16) |
-                     ((unsigned char)bytes[1] << 8) |
-                     (unsigned char)bytes[0]);
-    }
-
-    //Converts a two-character array to a short, using little-endian form
-    short toShort(const char* bytes)
-    {
-        return (short)(((unsigned char)bytes[1] << 8) |
-                       (unsigned char)bytes[0]);
-    }
-
-    //Reads the next four bytes as an integer, using little-endian form
-    int readInt(ifstream &input)
-    {
-        char buffer[4];
-        input.read(buffer, 4);
-        return toInt(buffer);
-    }
-
-    //Reads the next two bytes as a short, using little-endian form
-    short readShort(ifstream &input)
-    {
-        char buffer[2];
-        input.read(buffer, 2);
-        return toShort(buffer);
-    }
-
-    //Just like auto_ptr, but for arrays
-    template<class T>
-    class auto_array
-    {
-    private:
-        T* array;
-        mutable bool isReleased;
-    public:
-        explicit auto_array(T* array_ = NULL) :
-        array(array_), isReleased(false)
-        {
-        }
-
-        auto_array(const auto_array<T> &aarray)
-        {
-            array = aarray.array;
-            isReleased = aarray.isReleased;
-            aarray.isReleased = true;
-        }
-
-        ~auto_array()
-        {
-            if (!isReleased && array != NULL)
-            {
-                delete[] array;
-            }
-        }
-
-        T* get() const
-        {
-            return array;
-        }
-
-        T &operator*() const
-        {
-            return *array;
-        }
-
-        void operator=(const auto_array<T> &aarray)
-        {
-            if (!isReleased && array != NULL)
-            {
-                delete[] array;
-            }
-            array = aarray.array;
-            isReleased = aarray.isReleased;
-            aarray.isReleased = true;
-        }
-
-        T* operator->() const
-        {
-            return array;
-        }
-
-        T* release()
-        {
-            isReleased = true;
-            return array;
-        }
-
-        void reset(T* array_ = NULL)
-        {
-            if (!isReleased && array != NULL)
-            {
-                delete[] array;
-            }
-            array = array_;
-        }
-
-        T* operator+(int i)
-        {
-            return array + i;
-        }
-
-        T &operator[](int i)
-        {
-            return array[i];
-        }
-    };
-}
-
-Image* loadBMP(const char* filename)
-{
-    ifstream input;
-    input.open(filename, ifstream::binary);
-    assert(!input.fail() || !"Could not find file");
-    char buffer[2];
-    input.read(buffer, 2);
-    assert(buffer[0] == 'B' && buffer[1] == 'M' || !"Not a bitmap file");
-    input.ignore(8);
-    int dataOffset = readInt(input);
-
-    //Read the header
-    int headerSize = readInt(input);
-    int width;
-    int height;
-    switch (headerSize)
-    {
-        case 40:
-            //V3
-            width = readInt(input);
-            height = readInt(input);
-            input.ignore(2);
-            assert(readShort(input) == 24 || !"Image is not 24 bits per pixel");
-            assert(readShort(input) == 0 || !"Image is compressed");
-            break;
-        case 12:
-            //OS/2 V1
-            width = readShort(input);
-            height = readShort(input);
-            input.ignore(2);
-            assert(readShort(input) == 24 || !"Image is not 24 bits per pixel");
-            break;
-        case 64:
-            //OS/2 V2
-            assert(!"Can't load OS/2 V2 bitmaps");
-            break;
-        case 108:
-            //Windows V4
-            assert(!"Can't load Windows V4 bitmaps");
-            break;
-        case 124:
-            //Windows V5
-            assert(!"Can't load Windows V5 bitmaps");
-            break;
-        default:
-            assert(!"Unknown bitmap format");
-    }
-
-    //Read the data
-    int bytesPerRow = ((width * 3 + 3) / 4) * 4 - (width * 3 % 4);
-    int size = bytesPerRow * height;
-    auto_array<char> pixels(new char[size]);
-    input.seekg(dataOffset, ios_base::beg);
-    input.read(pixels.get(), size);
-
-    //Get the data into the right format
-    auto_array<char> pixels2(new char[width * height * 3]);
-    for (int y = 0; y < height; y++)
-    {
-        for (int x = 0; x < width; x++)
-        {
-            for (int c = 0; c < 3; c++)
-            {
-                pixels2[3 * (width * y + x) + c] =
-                pixels[bytesPerRow * y + 3 * x + (2 - c)];
-            }
-        }
-    }
-
-    input.close();
-    return new Image(pixels2.release(), width, height);
 }
